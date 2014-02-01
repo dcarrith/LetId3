0bbfb1ace66606e458a84f4798d773d89363aff6
Dave <dcarrith@gmail.com>
2014-01-31 23:29:50 -0500

    Updated the use statement for GetId3

--- src/Dcarrith/LetId3/LetId3.php
+++ src/Dcarrith/LetId3/LetId3.php
@@ -1,8 +1,8 @@
 <?php namespace Dcarrith\LetId3;
 
-use GetId3\GetId3Core;
+use \GetId3\GetId3Core as GetId3;
 
-class LetId3 extends GetId3Core {
+class LetId3 extends GetId3 {
 
         function __construct() { 
 
